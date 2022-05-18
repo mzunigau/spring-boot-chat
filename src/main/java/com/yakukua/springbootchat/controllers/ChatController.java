05f33d92fef790f7af539d4849eacc965e3f35f5
mzunigau <mazuga92@gmail.com>
2022-05-18 13:56:18 -0600

    Historial de chat

--- src/main/java/com/yakukua/springbootchat/controllers/ChatController.java
+++ src/main/java/com/yakukua/springbootchat/controllers/ChatController.java
@@ -1,8 +1,11 @@
 package com.yakukua.springbootchat.controllers;
 
 import com.yakukua.springbootchat.models.entities.Mensaje;
+import com.yakukua.springbootchat.models.service.IChatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
@@ -14,6 +17,12 @@ public class ChatController {
 
     private final String[] colores ={"red", "green", "blue", "orange", "magenta" ,"purple"};
 
+    @Autowired
+    private IChatService chatService;
+
+    @Autowired
+    private SimpMessagingTemplate webSocket;
+
     @MessageMapping("/mensaje")
     @SendTo("/chat/mensaje")
     public Mensaje recibeMensaje(Mensaje mensaje){
@@ -24,6 +33,8 @@ public class ChatController {
         if(mensaje.getTipo().equals("NUEVO_USUARIO")){
             mensaje.setColor(colores[new Random().nextInt(colores.length)]);
             mensaje.setTexto("Nuevo Usuario");
+        }else{
+            chatService.save(mensaje);
         }
 
         return mensaje;
@@ -35,6 +46,12 @@ public class ChatController {
     @SendTo("/chat/escribiendo")
     public String escribiendo(String username){
         return username.concat(" esta escribiendo...");
+    }
+
+    //Historial de mensajes dle chat
+    @MessageMapping("/historial")
+    public void historial(String clienteId){
+        webSocket.convertAndSend("/chat/historial/"+ clienteId, chatService.obtenerUltimos10Mensajes());
     }
 
 }
